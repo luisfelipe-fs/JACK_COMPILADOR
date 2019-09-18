93adca2bba90c24789a69a45798db340a66c96ad
luisfelipe-fs <51939387+luisfelipe-fs@users.noreply.github.com>
2019-09-18 13:51:09 -0300

    Correção de alguns símbolos
    Alguns símbolos foram escritos incorretamente, resultando em um XML diferente do esperado.

--- LexicalAnalyzer/Token.py
+++ LexicalAnalyzer/Token.py
@@ -1,7 +1,7 @@
 import re
 
 KEYWORDS = ['class', 'constructor', 'function', 'method', 'field', 'static', 'var', 'int', 'char', 'boolean', 'void', 'true', 'false', 'null', 'this', 'let', 'do', 'if', 'else', 'while', 'return']
-SYMBOLS = ['{', '}', '(', ')', '[', ']', '. ', ', ', '; ', '+', '-', '*', '/', '&', '|', '<', '>', '=', '~']
+SYMBOLS = ['{', '}', '(', ')', '[', ']', '.', ',', ';', '+', '-', '*', '/', '&', '|', '<', '>', '=', '~']
 INTEGER_REGEX = '[0-9]+'
 STRING_REGEX = '".*"'
 
