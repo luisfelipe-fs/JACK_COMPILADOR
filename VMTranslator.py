a74ece20d516c06ae1557a1ffad032b63b61f19d
luisfelipe-fs <51939387+luisfelipe-fs@users.noreply.github.com>
2019-12-12 03:26:09 -0300

    VMTranslator

---
+++ VMTranslator.py
@@ -0,0 +1,23 @@
+from VMTranslator.CodeWriter import CodeWriter
+from VMTranslator.Parser import Parser
+
+class VMTranslator:
+    def __init__ (self, path):
+        self.path = path.replace('\\', '/')
+        try:
+            self.filename = self.path[len(self.path)-self.path[::-1].index('/'):-3]
+        except ValueError:
+            self.filename = self.path[:-3]
+        self.cw = CodeWriter(self.filename)
+        self.parser = Parser(path)
+
+    def translate (self):
+        self.parser.advance()
+        for token in self.parser.tokens:
+            if token.getToken() in ['push', 'pop']:
+                self.cw.writePushPop(token.getToken(), token.getArg(0), token.getArg(1))
+            elif token.getToken() in self.parser.ARITHMETIC:
+                self.cw.writeArithmetic(token.getToken())
+        with open(self.path[:-3] + '.asm', 'w') as f:
+            f.write(self.cw.out)
+        print("Translate of '%s' done." % self.path)
